574c40427c9add0789390ccc3129c4aac5d1e8be
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-07 12:21:59 -0400

    Update example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -2,5 +2,5 @@ import pyshapes as ps
 
 radius, base, height = 5, 10, 7
 
-print(ps.circumference(radius))
-print(ps.triangle_area(base, height))
+print(ps.circumference(radius)) # returns 31.4
+print(ps.triangle_area(base, height)) # returns 35.0
