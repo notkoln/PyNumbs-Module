2a1f37b353587f60db04a01df85cfeb115338413
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-08 07:56:35 -0400

    Update example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -4,3 +4,5 @@ radius, base, height = 5, 10, 7
 
 print(ps.circumference(radius)) # returns 31.4
 print(ps.triangle_area(base, height)) # returns 35.0
+print(ps.sphere_surface_area(4)) # returns 200.96
+print(ps.pyramid_volume(5, 7, 3)) # returns 35
