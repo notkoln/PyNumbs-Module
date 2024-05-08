11bcda9e8387b4b63874f78496f4498ff8e341a0
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-08 07:51:58 -0400

    Update pyshapes.py

--- pyshapes.py
+++ pyshapes.py
@@ -4,3 +4,9 @@ def triangle_area(b,h):
   return 0.5*(b*h)
 def cone_volume(r,h):
   return (3.14*(r*r))*(h/3)
+def sphere_surface_area(r):
+  return (4*3.14)*(r*r)
+def sphere_volume(r):
+  return (1.33*3.14)*(r**3)
+def pyramid_volume(l,w,h):
+  return (l*w*h)/3
